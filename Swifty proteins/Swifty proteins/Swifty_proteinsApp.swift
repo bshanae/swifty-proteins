37b7e5f5542fb903556bd3063cce7a49b664e2f0
v.belchenko <belchenkovova@gmail.com>
2021-06-28 23:04:58 +0300

    Added template project

---
+++ Swifty proteins/Swifty proteins/Swifty_proteinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swifty_proteinsApp.swift
+//  Swifty proteins
+//
+//  Created by Vladimir Belchenko on 28.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Swifty_proteinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
