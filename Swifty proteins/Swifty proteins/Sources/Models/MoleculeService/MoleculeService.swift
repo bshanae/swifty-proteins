02a18444eb187c4b6c6b55d340daaaa32a70b559
Valeria Karon <vkaron@oa-i3.msk.21-school.ru>
2021-07-09 03:50:16 +0300

    fix load molecule - change from model to ideal file and change index of atom

--- Swifty proteins/Swifty proteins/Sources/Models/MoleculeService/MoleculeService.swift
+++ Swifty proteins/Swifty proteins/Sources/Models/MoleculeService/MoleculeService.swift
@@ -26,26 +26,26 @@ class MoleculeService {
 			var i = 1
 			var statusData = false
 			var atomCount = 0
-			var linkCount = 0
+			var boundCount = 0
 			var id = 1
 			for line in lines {
 				let info = line.components(separatedBy: .whitespaces).filter({ $0 != "" })
 				if (statusData == false) && (i == 4){
 					atomCount = Int(info[0]) ?? 0
-					linkCount = Int(info[1]) ?? 0
+					boundCount = Int(info[1]) ?? 0
 					statusData = true
 				} else if statusData && (i < 5 + atomCount){
 					atoms.append(Atom.parse(
-						id: id,
+						id: id - 1,
 						stringKind: String(info[3]),
 						x: Double(info[0]) ?? 0.0,
 						y: Double(info[1]) ?? 0.0,
 						z: Double(info[2]) ?? 0.0
 					))
 					id += 1
-				} else if statusData && (i < 5 + atomCount + linkCount) {
-					let atomId = Int(info[0]) ?? 0
-					let boundAtomId = (Int(info[1]) ?? 0)
+				} else if statusData && (i < 5 + atomCount + boundCount) {
+					let atomId = (Int(info[0]) ?? 1) - 1
+					let boundAtomId = (Int(info[1]) ?? 1) - 1
 					let typeOfBound = (Int(info[2]) ?? 0)
 					
 					bounds.append(
@@ -68,7 +68,7 @@ class MoleculeService {
 		ofMolecule moleculeId: String,
 		onResult: @escaping (_ data: String?, _ error: String?) -> ()
 	) {
-		let urlAsString = "https://files.rcsb.org/ligands/view/\(moleculeId.uppercased())_model.sdf"
+		let urlAsString = "https://files.rcsb.org/ligands/view/\(moleculeId.uppercased())_ideal.sdf"
 		guard let urlToGet = URL(string: urlAsString) else {
 			onResult(nil, "Can't create URL")
 			return
