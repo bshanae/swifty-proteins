749d15fdd02ccb6a4b79be0340a945daf1e23e3d
Valeria Karon <vkaron@oa-k1.msk.21-school.ru>
2021-07-11 01:38:20 +0300

    add icons, add logo, add splash, add FaceID

--- Swifty proteins/Swifty proteins/Sources/Views/ContentView.swift
+++ Swifty proteins/Swifty proteins/Sources/Views/ContentView.swift
@@ -1,9 +1,77 @@
 import SwiftUI
+import LocalAuthentication
 
-struct ContentView: View {
-	var body: some View {
-		MoleculeListPage()
-	}
+
+struct IntroView: View {
+    
+    enum AccessError {
+        case notProblem
+        case noFace
+        case noBiometrics
+    }
+    
+    @State var isActive:Bool = false
+    
+    @State private var isUnlocked = false
+    
+    @State private var accessError:AccessError = .notProblem
+    
+    let splashDelay = 2.5
+    
+    var body: some View {
+        ZStack {
+            if self.isActive {
+                VStack {
+                    if self.isUnlocked {
+                        MoleculeListPage()
+                    } else {
+                        ZStack {
+                            VStack {
+                                if self.accessError == .noFace {
+                                    Text("No Face")
+                                } else if self.accessError == .noBiometrics {
+                                    Text("No Biometrics")
+                                }
+                                Image(systemName: "lock.slash")
+                            }
+                        }
+                    }
+                }
+                .onAppear(perform: authenticate)
+            } else {
+                Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            }
+        }.onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + self.splashDelay) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        self.accessError = .noFace
+                    }
+                }
+            }
+        } else {
+            self.accessError = .noBiometrics
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
