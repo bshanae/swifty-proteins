c3f4b2b8bda67a8c65b8e316ca517bdea3f67fd8
Valeria Karon <vkaron@oa-i5.msk.21-school.ru>
2021-07-14 03:53:42 +0300

    fix parser

--- Swifty proteins/Sources/Models/MoleculeService/MoleculeService.swift
+++ Swifty proteins/Sources/Models/MoleculeService/MoleculeService.swift
@@ -21,42 +21,44 @@ class MoleculeService {
 	private static func parseMolecule(moleculeId: String, file: String) -> Molecule {
 		var atoms = [Atom]()
 		var bounds = [Bound]()
-		
 		let lines = file.components(separatedBy: .newlines)
 		if lines[0] == moleculeId.uppercased() {
 			var i = 1
-			var statusData = false
-			var atomCount = 0
-			var boundCount = 0
-			var id = 1
+			var id = 0
 			for line in lines {
 				let info = line.components(separatedBy: .whitespaces).filter({ $0 != "" })
-				if (statusData == false) && (i == 4){
-					atomCount = Int(info[0]) ?? 0
-					boundCount = Int(info[1]) ?? 0
-					statusData = true
-				} else if statusData && (i < 5 + atomCount){
-					atoms.append(Atom.parse(
-						id: id - 1,
-						stringKind: String(info[3]),
-						x: Double(info[0]) ?? 0.0,
-						y: Double(info[1]) ?? 0.0,
-						z: Double(info[2]) ?? 0.0
-					))
-					id += 1
-				} else if statusData && (i < 5 + atomCount + boundCount) {
-					let atomId = (Int(info[0]) ?? 1) - 1
-					let boundAtomId = (Int(info[1]) ?? 1) - 1
-					let typeOfBound = (Int(info[2]) ?? 0)
-					
-					bounds.append(
-						Bound.parse(
-							atomId: atomId,
-							boundAtomId: boundAtomId,
-							typeOfBound: typeOfBound
+				if (i > 4) {
+                    if String(info[0]).isEqual("M") {
+                        break
+                    }
+                    if info.count > 7 {
+                        atoms.append(Atom.parse(
+							id: id,
+							stringKind: String(info[3]),
+							x: Double(info[0]) ?? 0.0,
+							y: Double(info[1]) ?? 0.0,
+							z: Double(info[2]) ?? 0.0
+						))
+						id += 1
+                    } else {
+                        let index1 = line.index(line.startIndex, offsetBy: 3)
+                        let index2 = line.index(line.startIndex, offsetBy: 6)
+                        let index3 = line.index(line.startIndex, offsetBy: 9)
+                        var tmp = String(line[..<index1]).trimmingCharacters(in: .whitespacesAndNewlines)
+                        let atomId = Int(tmp) ?? 0
+                        tmp = String(line[index1..<index2]).trimmingCharacters(in: .whitespacesAndNewlines)
+                        let boundAtomId = Int(tmp) ?? 0
+                        tmp = String(line[index2..<index3]).trimmingCharacters(in: .whitespacesAndNewlines)
+                        let typeOfBound = Int(tmp) ?? 0
+                        bounds.append(
+							Bound.parse(
+								atomId: atomId,
+								boundAtomId: boundAtomId,
+								typeOfBound: typeOfBound
+							)
 						)
-					)
-				}
+                    }
+                }
 				i += 1
 			}
 		}
